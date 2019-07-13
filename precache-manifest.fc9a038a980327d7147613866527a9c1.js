85c6904f5f1a123777615524c543da26e31613fa
javier <javierenriquemendozagomez@gmail.com>
2019-07-13 20:29:36 +0100

    Updates

---
+++ precache-manifest.fc9a038a980327d7147613866527a9c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd96f1b30af681f1c6bbc1555d3cf2b6",
+    "url": "/monsters-rodolex/index.html"
+  },
+  {
+    "revision": "b891e461d79afaa55632",
+    "url": "/monsters-rodolex/static/css/main.3be3272f.chunk.css"
+  },
+  {
+    "revision": "458a39bd49d85bb2560b",
+    "url": "/monsters-rodolex/static/js/2.b0d4d72d.chunk.js"
+  },
+  {
+    "revision": "b891e461d79afaa55632",
+    "url": "/monsters-rodolex/static/js/main.fd932ecf.chunk.js"
+  },
+  {
+    "revision": "a9260227d0878e8e1bc9",
+    "url": "/monsters-rodolex/static/js/runtime~main.6df6c14a.js"
+  }
+]);
